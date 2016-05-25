353341c4e1c6bd11043ee8577c72ccefb7d55d99
Cyrille Médard de Chardon <cyrillem@gmail.com>
2016-05-25 12:40:20 +0100

    JCDecaux enconding pain

--- parsers/pyBSRP/bsrp/protocols/jcdecaux.py
+++ parsers/pyBSRP/bsrp/protocols/jcdecaux.py
@@ -10,7 +10,7 @@ def parse(df, data, utc):
     # parse out desired info
     # does the file have valid content
     try:
-        json_data = json.loads(data, 'utf-8')
+        json_data = json.loads(unicode(data, 'iso-8859-1'))
     except ValueError:
         print utc + ' ' + df['bssid'] + " Parsing JSON failed for " + df['feedurl']
         return False
