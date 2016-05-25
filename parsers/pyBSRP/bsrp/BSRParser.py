08737eafee431f93f2f35c44893ff324f4f563d8
Cyrille Médard de Chardon <cyrillem@gmail.com>
2016-05-25 14:07:01 +0100

    Module difficulty

--- parsers/pyBSRP/bsrp/BSRParser.py
+++ parsers/pyBSRP/bsrp/BSRParser.py
@@ -17,6 +17,7 @@ class BSRParser(object):
             return
 
         # Load the appropriate parser module
+        print sys.modules['protocol']
         del sys.modules['protocol']
         self.proto = imp.load_source('protocol', os.path.dirname(os.path.abspath(__file__)) + '/protocols/' + bsr_feed['parsername'] + '.py')
         #self.proto = imp.load_source('protocol', '/protocols/' + bsr_feed['parsername'] + '.py')
